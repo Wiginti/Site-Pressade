312bbe6a68e6dd9e45f66b12147cbc953d66282a
Wiginti <marcodupre60@gmail.com>
2022-03-11 12:24:59 +0100

    nav burger open btn event added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,9 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  let btn = document.getElementById("btn-burger");
+  let items = document.querySelectorAll("nav li");
+  btn.addEventListener("click", () => {
+    for (const item of items) {
+      item.classList.toggle("hidden");
+    }
+  });
+});
