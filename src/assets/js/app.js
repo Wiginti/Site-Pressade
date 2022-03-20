cc772132ff4377bdc78d6af42885eb501a7054e1
Wiginti <marcodupre60@gmail.com>
2022-03-20 23:14:30 +0100

    ajout du responsif placement font size et img

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,22 +2,18 @@ window.addEventListener("DOMContentLoaded", () => {
   let btnUp = document.getElementById("btn-burger");
   let btnClose = document.getElementById("btn-close");
   let items = document.querySelectorAll("nav li");
-
-  function hideToggle(x) {
-    x.classList.toggle("hidden");
-  }
   btnUp.addEventListener("click", () => {
     for (const item of items) {
-      hideToggle(item);
-      hideToggle(btnClose);
-      hideToggle(btnUp);
+      item.classList.toggle("hidden");
     }
+    btnClose.classList.toggle("hidden");
+    btnUp.classList.toggle("hidden");
   });
   btnClose.addEventListener("click", () => {
     for (const item of items) {
-      hideToggle(item);
-      hideToggle(btnClose);
-      hideToggle(btnUp);
+      item.classList.toggle("hidden");
     }
+    btnClose.classList.toggle("hidden");
+    btnUp.classList.toggle("hidden");
   });
 });
